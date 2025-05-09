458a1175a272dffbb7069fddeb57d22a07694703
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2025-05-09 16:46:11 +0300

    hw1

---
+++ script.js
@@ -0,0 +1,84 @@
+// Завдання 1
+let q;
+let r;
+let s;
+let d;
+let e;
+let f;
+let g;
+let k;
+let l;
+let m;
+let n;
+let o;
+let p;
+
+q = "hello";
+
+r = "owu";
+
+s = "com";
+
+d = "ua";
+
+e = 1;
+
+f = 10;
+
+g = -999;
+
+k = 123;
+
+l = 3.14;
+
+m = 2.7;
+
+n = 16;
+
+o = true;
+
+p = false;
+console.log(" q:", q);
+console.log(" r:", r);
+console.log(" s:", s);
+console.log(" d:", d);
+console.log(" e:", e);
+console.log(" f:", f);
+console.log(" g:", g);
+console.log(" k:", k);
+console.log(" l:", l);
+console.log(" m:", m);
+console.log(" n:", n);
+console.log(" o:", o);
+console.log(" p:", p);
+
+// Завдання 2
+
+let firstName;
+let middleName;
+let lastName;
+firstName = "Оксана";
+middleName = "Ярославівна";
+lastName = "Москальова";
+// let person = `${firstName} ${middleName} ${lastName}`;
+let person = firstName + " " + middleName + " " + lastName;
+console.log(" person:", person);
+
+// Завдання 3
+let a = 100;
+let b = "100";
+let c = true;
+console.log("тип a:", typeof a);
+console.log("тип b:", typeof b);
+console.log("тип c:", typeof c);
+
+// Завдання 4
+
+let myFirstName = prompt("Enter Your Firstname");
+console.log("myFirstName:", myFirstName);
+let myLastName = prompt("Enter Your Lastname");
+console.log(" myLastName:", myLastName);
+let age = prompt("Enter Your Age");
+console.log(" age:", age);
+let myPerson = `I am ${myFirstName} ${myLastName}. I am ${age} years old.`;
+console.log(" myPerson :", myPerson);
