986e6cec4724aaf3eb64fc7f08af1f1bc2318c9e
OksanaMosk <oks-m3@ukr.net>
2025-05-19 20:46:59 +0300

    3_4

--- hw_3/hw_3_3/hw_3_3.js
+++ hw_3/hw_3_3/hw_3_3.js
@@ -1,12 +1,11 @@
 let i = 0;
-while (i <= 20) {
+while (i < 20) {
   if (document.readyState === "loading") {
     document.write(`<h1>Okten is cool ${i}</h1>`);
   } else {
     const container = document.createElement("div");
-    container.innerHTML = `<h1>Okten is cool ${i}</h1>`;
+    container.innerHTML = `<h1>Okten is cool</h1>`;
     document.body.appendChild(container);
   }
-    i++;
+  i++;
 }
-
