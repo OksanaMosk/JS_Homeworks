3a3bf0c2bfbe92ec7584e15895f5271febf97f9e
OksanaMosk <oks-m3@ukr.net>
2025-05-27 23:51:12 +0300

    10_10

--- hw_10/hw_10_10/hw_10_10.js
+++ hw_10/hw_10_10/hw_10_10.js
@@ -89,173 +89,15 @@ const arrayMovie = [
       "Years ago, the fearsome Pirate King, Gol D. Roger …get the One Piece and become King of the Pirates!",
     image: "https://image.tmdb.org/t/p/w500/2rmK7mnchw9Xr3XdiTFSxTTLXqv.jpg",
   },
-
-  {
-    title: "New",
-    id: 1275232,
-    original_language: "ru",
-    genre_ids: [18],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/8OGYrp1Q9WabUZ8TWmAML7tctR.jpg",
-  },
-  {
-    title: "NEW",
-    id: 367531,
-    original_language: "en",
-    genre_ids: [18, 878],
-    adult: false,
-    backdrop_full_path: null,
-  },
-  {
-    title: "NEW",
-    id: 865130,
-    original_language: "ja",
-    genre_ids: [],
-    adult: false,
-    backdrop_full_path: null,
-  },
-  {
-    title: "Lassie: A New Adventure",
-    id: 1088563,
-    original_language: "de",
-    genre_ids: [12, 10751],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/9utq8PrMyvFBtlte2BDNaj1ktSi.jpg",
-  },
-  {
-    title: "Pornocracy: The New Sex Multinationals",
-    id: 438478,
-    original_language: "fr",
-    genre_ids: [99],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/2VFUh8spzyNbRNPyncIrFmSwBrd.jpg",
-  },
-  {
-    title: "Captain America: Brave New World",
-    id: 822119,
-    original_language: "en",
-    genre_ids: [28, 53, 878],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/8eifdha9GQeZAkexgtD45546XKx.jpg",
-  },
-  {
-    title: "New Golden Lotus II",
-    id: 492104,
-    original_language: "cn",
-    genre_ids: [],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/kQBIdDt5SLX3HqUtnqOybWXnCSQ.jpg",
-  },
-  {
-    title: "new.",
-    id: 1260886,
-    original_language: "en",
-    genre_ids: [],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/sx4onZy5xLCpD45DsyuZKt6EGSn.jpg",
-  },
-  {
-    title: "Brave New World",
-    id: 27260,
-    original_language: "en",
-    genre_ids: [878, 10770],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/inRUNgkrVdri9C12lgvqfds1qTn.jpg",
-  },
-  {
-    title: "New",
-    id: 1311550,
-    original_language: "en",
-    genre_ids: [2],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/c3rwwFFVbkyEI6wPtpPd9lvovPW.jpg",
-  },
-  {
-    title: "NEW",
-    id: 1049948,
-    original_language: "en",
-    genre_ids: [1],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/4Ep2KivIBUZbkS7kHjW7Qywnhhj.jpg",
-  },
-  {
-    title: "NEW",
-    id: 1279104,
-    original_language: "en",
-    genre_ids: [2],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/rZ8VxBH8QRHGQi9YztBRm3eAsxL.jpg",
-  },
-  {
-    title: "Lassie: A New Adventure",
-    id: 1098006,
-    original_language: "en",
-    genre_ids: [3],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/aESb695wTIF0tB7RTGRebnYrjFK.jpg",
-  },
-  {
-    title: "NEW",
-    id: 896536,
-    original_language: "en",
-    genre_ids: [3],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/dJ1JpHuTCRxtGpRq8bQzk2jBqTi.jpg",
-  },
-  {
-    title: "NEW",
-    id: 729165,
-    original_language: "en",
-    genre_ids: [4],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/wPNI1fd18z5TKBOK9Mv9Rfjb0j0.jpg",
-  },
-  {
-    title: "NEW",
-    id: 836225,
-    original_language: "en",
-    genre_ids: [4],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/b1L7qevxiVpkVFq4dmdQPGFPWH0.jpg",
-  },
-  {
-    title: "NEW",
-    id: 1373723,
-    original_language: "en",
-    genre_ids: [2],
-    adult: false,
-    backdrop_full_path:
-      "https://image.tmdb.org/t/p/w500/ibF5XVxTzf1ayzZrmiJqgeQ39Qk.jpg",
-  },
-  {
-    title: "NEW",
-    id: 1264966,
-    original_language: "en",
-    genre_ids: [2],
-    adult: false,
-    image: "https://image.tmdb.org/t/p/w500/mo1MwTW7P9byv4O4w4he3DngyxW.jpg",
-  },
 ];
+
+const butPrev = document.getElementsByTagName("button")[0];
+const butNext = document.getElementsByTagName("button")[1];
 
 let listOfMovie = document.createElement("ul");
 listOfMovie.classList.add("items");
 for (let i = 0; i < arrayMovie.length; i++) {
   let item = document.createElement("li");
-  item.innerText = arrayMovie[i].name;
 
   item.classList.add("everyItem");
 
@@ -270,4 +112,5 @@ for (let i = 0; i < arrayMovie.length; i++) {
   item.append(itemName, itemImg);
   listOfMovie.append(item);
 }
-document.body.append(listOfMovie);
+
+butPrev.insertAdjacentElement("afterend", listOfMovie);
